72615afa0432ababb89a57df0fde2ec55ecd6596
DoItYsf <42316880+DoItYsf@users.noreply.github.com>
2022-01-06 21:43:08 +0800

    Add files via upload

---
+++ 预处理器/1.2 #define/1-2-3.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 1-2-2.c
+	> Author: DoItYsf
+	> Mail: javathon@hotmail.com
+	> Created Time: 2021年12月22日 星期三 13时14分29秒
+ ************************************************************************/
+
+#define MAX(x,y) x>y?x:y
+#include<stdio.h>
+int main(){
+    int a = 1,b = 2;
+    printf("max(a,b) = %d\n",MAX(a));
+    return 0;
+}
---
+++ 预处理器/1.3 #、##/1-3-1.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: 1-3-1.c
+	> Author: DoItYsf
+	> Mail: javathon@hotmail.com
+	> Created Time: 2022年01月06日 星期四 12时00分35秒
+ ************************************************************************/
+
+#include<stdio.h>
+#define print(...) printf(#__VA_ARGS__)
+int main(){
+    print(hello,world\n);
+    return 0;
+}
